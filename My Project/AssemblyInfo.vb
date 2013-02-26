b8f0417060fe20125c3f367de57079dcf77b0ed9
BenChen <ben.chenoweth@gmail.com>
2013-02-26 22:56:21 +1100

    2.0.0 - Update to latest version of InKey

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("A GUI for creating keyboards for InKey")> 
 <Assembly: AssemblyCompany("SIL")> 
 <Assembly: AssemblyProduct("InKeyKeyboardCreator")> 
-<Assembly: AssemblyCopyright("Copyright © SIL 2008-2012")> 
+<Assembly: AssemblyCopyright("Copyright © SIL 2008-2013")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.9.6")> 
-<Assembly: AssemblyFileVersion("1.9.6")> 
+<Assembly: AssemblyVersion("2.0.0")> 
+<Assembly: AssemblyFileVersion("2.0.0")> 
